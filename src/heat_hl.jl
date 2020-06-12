b53b5ace0d1006619c1172876baf28ba71c8c4ee
aitzkora <marc.fuentes@aquilenet.fr>
2020-06-12 17:23:07 +0200

    add the "inverse" of ins

--- src/heat_hl.jl
+++ src/heat_hl.jl
@@ -1,4 +1,5 @@
 using LinearAlgebra
+using Test
 
 function heat_kernel(X::Array{Float64,2})
     Y = copy(X)
@@ -11,12 +12,6 @@ function heat_kernel(X::Array{Float64,2})
     return Y
 end
 
-
-function ins(X::Array{Float64,1})
-    return X[2:end-1,2:end-1][:]
-end
-
-using Test
 @testset "kernel heat" begin
     a = ones(5,5)
     a[2:end-1,2:end-1] .= 0.
@@ -24,6 +19,24 @@ using Test
     res[2:end-1,2:end-1] = [ -2 -1 -2; -1 0 -1; -2 -1 -2 ]
     @test heat_kernel(a) ≈ res
 end
+
+function ins(X::Array{Float64,2})
+    return X[2:end-1,2:end-1][:]
+end
+
+
+function ins⁻¹(x::Array{Float64,1})
+    n = convert(Int64, √size(x,1))
+    X=zeros(n+2,n+2)
+    X[2:end-1,2:end-1]=x
+    return X
+end
+
+@testset "ins"  begin
+    x = rand(9)
+    @test ins(ins⁻¹(x)) == X
+end
+
 
 function apply_Δ(x::Array{Float64,1}, g::Array{Float64,1})
     n = convert(Int64, √size(x,1))
@@ -35,7 +48,7 @@ function apply_Δ(x::Array{Float64,1}, g::Array{Float64,1})
     X[2:end-1,end] = g[n+4:2:3n+2]
     X[end,:] = g[3n+3:4n+4]
     Y = heat_kernel(X) / (n+1).^2
-    return copy(Y[2:end-1,2:end-1][:])
+    return copy(ins(Y))
 end
 
 function get_boundary(X::Array{Float64,2})
@@ -56,8 +69,8 @@ end
 end
 
 @testset "apply Δ" begin
-    n = 4
-    x = LinRange(0,1,n+2)
+    n = 1000
+    x = linrange(0,1,n+2)
     x = reshape(repeat(x, inner=n+2), n+2, n+2)
     y = copy(x)
     u = y.*(1 .- y).*x.^3
